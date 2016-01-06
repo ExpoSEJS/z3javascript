6cbe83bac2a8f435ec4b639fe606bac7925ad163
Blake Loring <blake_l@parsed.uk>
2016-01-06 14:36:16 -0800

    Change build scripts to remote -m32 flags

--- scripts/build_z3.sh
+++ scripts/build_z3.sh
@@ -2,9 +2,10 @@
 ./emsdk/emsdk_env.sh
 
 cd z3
-if [ -e build ]; then
-    mv build build-$(stat -c "%y" build | sed 's/[ :]/_/g' - | sed 's/\..*/ /g')
-fi
+
+#if [ -e build ]; then
+#    mv build build-$(stat -c "%y" build | sed 's/[ :]/_/g' - | sed 's/\..*/ /g')
+#fi
 
 alias c++=em++
 alias g++=em++
@@ -14,11 +15,14 @@ alias gcc=emcc
 alias cmake=emcmake
 alias configure=emconfigure
 alias ranlib=emranlib
+
 export CC=emcc
 export CXX=em++
+
 python scripts/mk_make.py --x86 --githash=$(git rev-parse HEAD) --staticlib
 cd build
 sed -i 's/EXE_EXT=/EXE_EXT=.emscripten.js/g' config.mk
+sed -i 's/-m32//g' config.mk
 sed -i 's/^\(LINK_EXTRA_FLAGS=.*\)/\1 -O3/g' config.mk
 emmake make
 cp z3.emscripten.js* ../../compiled/
