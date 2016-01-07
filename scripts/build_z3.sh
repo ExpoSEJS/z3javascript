3dd8a8ba184df90f1e46fbf66bdc5877eeafaa1a
Blake Loring <blake_l@parsed.uk>
2016-01-07 01:10:19 -0800

    Fix build script

--- scripts/build_z3.sh
+++ scripts/build_z3.sh
@@ -3,9 +3,9 @@
 
 cd z3
 
-#if [ -e build ]; then
-#    mv build build-$(stat -c "%y" build | sed 's/[ :]/_/g' - | sed 's/\..*/ /g')
-#fi
+if [ -e build ]; then
+    rm -rf ./build/
+fi
 
 alias c++=em++
 alias g++=em++
@@ -22,7 +22,8 @@ export CXX=em++
 python scripts/mk_make.py --x86 --githash=$(git rev-parse HEAD) --staticlib
 cd build
 sed -i 's/EXE_EXT=/EXE_EXT=.emscripten.js/g' config.mk
-sed -i 's/-m32//g' config.mk
-sed -i 's/^\(LINK_EXTRA_FLAGS=.*\)/\1 -O3/g' config.mk
+sed -i 's/-m32/-m64/g' config.mk
+sed -i 's/^\(CXXFLAGS=.*\)/\1 -fPIC/g' config.mk
+sed -i 's/^\(LINK_EXTRA_FLAGS=.*\)/\1 -O3 -fPIC/g' config.mk
 emmake make
 cp z3.emscripten.js* ../../compiled/
