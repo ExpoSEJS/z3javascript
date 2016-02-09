773c7a33a8c62bbf20587f9402f5836ffe8474c4
Blake Loring <blake_l@parsed.uk>
2016-02-09 15:26:35 +0000

    Fix the OS X build by removing /opt/bin from the PATH

--- scripts/build_z3.sh
+++ scripts/build_z3.sh
@@ -21,15 +21,20 @@ alias ranlib=emranlib
 
 export CC=em
 export CXX=em++
+export AR=emar
+export RANLIB=emranlib
+
+#TODO: Why the hell does this fix the Mac Build
+export PATH=`echo $PATH | sed -e 's/:\/opt\/local\/bin:\/opt\/local\/sbin//'`
 
 python scripts/mk_make.py --x86 --githash=$(git rev-parse HEAD) --staticlib
 cd build
-sed -i 's/EXE_EXT=/EXE_EXT=.emscripten.js/g' config.mk
-sed -i 's/-m32/-m64/g' config.mk
-sed -i 's/^\(CXXFLAGS=.*\)/\1 -fPIC -std=c++03 /g' config.mk
-sed -i 's/^\(CXXFLAGS=.*\)/\1 INSERTEXPORTHERE/g' config.mk
-sed -i 's/^\(LINK_EXTRA_FLAGS=.*\)/\1 -O3 -std=c++03 -fPIC INSERTEXPORTHERE/g' config.mk
-sed -i 's/O3/O3/g' config.mk
-sed -i "s/INSERTEXPORTHERE/-s EXPORTED_FUNCTIONS=\"[$(cat ../../compiled/z3_bindings_flat)]\"/g" config.mk
+sed -i.bak -e 's/EXE_EXT=/EXE_EXT=.emscripten.js/g' config.mk
+sed -i.bak -e 's/-m32/-m64/g' config.mk
+sed -i.bak -e 's/^\(CXXFLAGS=.*\)/\1 -fPIC -std=c++03 /g' config.mk
+sed -i.bak -e 's/^\(CXXFLAGS=.*\)/\1 INSERTEXPORTHERE/g' config.mk
+sed -i.bak -e 's/^\(LINK_EXTRA_FLAGS=.*\)/\1 -O3 -std=c++03 -fPIC INSERTEXPORTHERE/g' config.mk
+sed -i.bak -e 's/O3/O3/g' config.mk
+sed -i.bak -e "s/INSERTEXPORTHERE/-s EXPORTED_FUNCTIONS=\"[$(cat ../../compiled/z3_bindings_flat)]\"/g" config.mk
 emmake make -j 8
 cp z3.emscripten.js* ../../compiled/
