febc9131b9a2883820af07e1c3369ae392be46df
Blake Loring <blake_l@parsed.uk>
2016-03-30 00:31:25 +0100

    Z3 lib now returns a function which loads either native or js build depending on how it is invoked

--- templates/package.js
+++ templates/package.js
@@ -1,2 +1,8 @@
-import Z3 from './z3_bindings_em_es5.js';
-export default Z3;
+export default function(mode) {
+	if (mode === 'native') {
+		throw 'Unfinished';
+	}
+
+	import Z3 from './z3_bindings_em_es5.js';
+	return Z3;
+};
