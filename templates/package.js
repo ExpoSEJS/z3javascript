168ff49d735515ab230938a2e2d92a82c27c1278
Blake Loring <blake_l@parsed.uk>
2016-03-31 21:20:06 +0100

    Forgot to name the package default

--- templates/package.js
+++ templates/package.js
@@ -1,7 +1,9 @@
 export default function(mode, path) {
 
 	if (mode === 'native') {
-		return require('./z3_bindings_ref_es5.js').default(path);
+		return {
+			default: require('./z3_bindings_ref_es5.js').default(path)
+		};
 	}
 
 	return require('./z3_bindings_em_es5.js').default;
