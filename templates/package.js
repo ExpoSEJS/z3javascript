7221aab85d8fc0a8899d6bff46a4ec09f635a931
Blake Loring <blake_l@parsed.uk>
2016-06-21 16:51:53 +0100

    Tweaks to building Z3

--- templates/package.js
+++
@@ -1,8 +0,0 @@
-export default function(mode, path) {
-
-	if (mode === 'native') {
-		return require('./z3_bindings_ref_es5.js').default(path);
-	}
-
-	return require('./z3_bindings_em_es5.js').default;
-};
