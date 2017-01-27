68b81124dc74e410369795967c72ebaf9471f837
= <blake_l@parsed.uk>
2017-01-27 04:11:28 -0800

    Identify minimum failing case for this odd alternation

--- src/Z3Test.js
+++ src/Z3Test.js
@@ -10,10 +10,8 @@ var solver = new Z3.Solver(ctx);
 
 console.log('Compiling RegEx');
 
-let regExToTest = [/^(abcd)+(efg)(hijk((lmnop)|(qrs)))$/];
+let regExToTest = [/^(z((x)|(y)))$/];
 let testRegexs = regExToTest.map(r => Z3.Regex(ctx, r));
-
-console.log('Test Regex: ' + JSON.stringify(testRegexs));
 
 let symbol = ctx.mkStringSymbol('HI');
 let symbolic = ctx.mkConst(symbol, ctx.mkStringSort());
