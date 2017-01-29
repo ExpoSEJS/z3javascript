e3b19b522cd74492962713afca0c19a5c2d4c517
= <blake_l@parsed.uk>
2017-01-29 14:52:37 -0800

    Add regex unit tests and identify some failing test cases

---
+++ src/RegexUnitTests.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright Blake Loring <blake_l@parsed.uk> 2015
+ */
+"use strict";
+
+import Z3 from './Z3';
+
+var ctx = new Z3.Context();
+var solver = new Z3.Solver(ctx);
+
+function TestCaptures(regex, given, expectedCaptures) {
+	solver.push();
+	let compiled = Z3.Regex(ctx, regex);
+	let symbolic = ctx.mkConst(ctx.mkStringSymbol('capture'), ctx.mkStringSort());
+
+	//Force capture[0] -> given
+	if (given) {
+		solver.assert(ctx.mkEq(compiled.implier, ctx.mkString(given)).simplify());
+	}
+
+	solver.assert(ctx.mkSeqInRe(symbolic, compiled.ast));
+	solver.assert(ctx.mkImplies(ctx.mkSeqInRe(symbolic, compiled.ast), ctx.mkEq(symbolic, compiled.implier)));
+
+	compiled.assertions.forEach(assert => {
+		solver.assert(assert.simplify());
+	});
+
+	let mdl = solver.getModel();
+
+	if (mdl) {
+		if (!regex.test(mdl.eval(symbolic).asConstant())) {
+			return false;
+		} else {
+			let failed = false;
+
+			expectedCaptures.forEach((x, idx) => {
+				if (mdl.eval(compiled.captures[idx]).asConstant() != x) {
+					console.log('MDL.eval(' + mdl.eval(compiled.captures[idx]).asConstant() + ') != ' + x + ' expected');
+					failed = true;
+				}
+			});
+
+			if (failed) {
+				return false;
+			}
+		}
+	} else {
+		console.log('Unsat');
+		return false;
+	}
+
+	solver.pop();
+
+	return true;
+}
+
+function Assert(regex, given, expectedCaptures) {
+	if (!TestCaptures(regex, given, expectedCaptures || [])) {
+		throw 'Regex ' + regex + ' failed test given ' + given + ' ExpectedCap ' + expectedCaptures;
+	} else {
+		console.log('Regex ' + regex + ' passed test given ' + given + ' ExpectedCap ' + expectedCaptures);
+	}
+}
+
+Assert(/xxx/, 'xxx');
+Assert(/(xxx)/, 'xxx', ['xxx']);
+Assert(/^([a-zA-Z])+ef$/, 'def', ['def', 'e']);
+Assert(/^([a-zA-Z]){5}ef$/, 'abzCDef', ['abzCDef', 'D']);
+
--- src/Z3Test.js
+++ src/Z3Test.js
@@ -10,15 +10,14 @@ var solver = new Z3.Solver(ctx);
 
 console.log('Compiling RegEx');
 
-let regExToTest = [/^([a-zA-Z])*$/];
+let regExToTest = [/^([a-zA-Z])+ef$/];
 let testRegexs = regExToTest.map(r => Z3.Regex(ctx, r));
 
 let symbol = ctx.mkStringSymbol('HI');
 let symbolic = ctx.mkConst(symbol, ctx.mkStringSort());
 
 
-solver.assert(ctx.mkEq(testRegexs[0].implier, ctx.mkString('Q')).simplify());
-solver.assert(ctx.mkNot(ctx.mkEq(testRegexs[0].captures[0], ctx.mkString('Q'))).simplify());
+solver.assert(ctx.mkEq(testRegexs[0].captures[1], ctx.mkString('f')).simplify());
 
 console.log(testRegexs[0].captures[0].toString());
 
