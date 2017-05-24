e82ad3419d1c4469041037bcef0cd0235e7ce16a
Blake Loring <blake_l@parsed.uk>
2017-05-24 13:19:28 +0100

    Modify to select specific Z3

--- src/Regex.js
+++ src/Regex.js
@@ -286,6 +286,8 @@ function RegexRecursive(ctx, regex, idx) {
                     case '*':
                         {
                             //If anything the capture is optional then anything inside it is also optional
+                            //TODO: Take a list of originals and rewrite an implication
+                            //iff Len(origin) > 0 then c[i] = o[i]
                             for (let i = newestCapture; i < captures.length; i++) {
                                 rewriteCaptureOptional(i);
                             }
