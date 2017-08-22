e666dba2221b365ae02f4372dd942ba990bb82d4
Blake Loring <bloring@cloudflare.com>
2017-08-22 13:33:12 +0100

    FE not FF

--- src/Regex.js
+++ src/Regex.js
@@ -68,7 +68,7 @@ function RegexRecursive(ctx, regex, idx) {
 
     function Any() {
         let beforeNewline = ctx.mkReRange(ctx.mkString('\\x00'), ctx.mkString('\\x09'));
-        let afterNewline = ctx.mkReRange(ctx.mkString('\\x0B'), ctx.mkString('\\x79'));
+        let afterNewline = ctx.mkReRange(ctx.mkString('\\x0B'), ctx.mkString('\\xFE'));
         return ctx.mkReUnion(beforeNewline, afterNewline);
     }
 
@@ -77,7 +77,7 @@ function RegexRecursive(ctx, regex, idx) {
      * This will accept any character
      */
     function TruelyAny() {
-        return ctx.mkReRange(ctx.mkString('\\x00'), ctx.mkString('\\xFF'));
+        return ctx.mkReRange(ctx.mkString('\\x00'), ctx.mkString('\\xFE'));
     }
 
     function ParseRangerNextEscaped() {
